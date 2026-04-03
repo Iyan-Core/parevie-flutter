447377f607bafc299cd4f4ac84784eb36ae0840d
Iyan-Core <awianton2@gmail.com>
2026-04-03 22:01:59 +0700

    Create titipan.dart

---
+++ lib/models/titipan.dart
@@ -0,0 +1,17 @@
+class Titipan {
+  String id;
+  String tokoId;
+  String produkId;
+  int qty;
+  DateTime tglMasuk;
+  DateTime lastCek;
+
+  Titipan({
+    required this.id,
+    required this.tokoId,
+    required this.produkId,
+    required this.qty,
+    required this.tglMasuk,
+    required this.lastCek,
+  });
+}
