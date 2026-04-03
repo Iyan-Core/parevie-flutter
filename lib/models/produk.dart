60e76ad0c24db6e48d6ec12a31688686317fd370
Iyan-Core <awianton2@gmail.com>
2026-04-03 22:01:07 +0700

    Create produk.dart

---
+++ lib/models/produk.dart
@@ -0,0 +1,11 @@
+class Produk {
+  String id;
+  String nama;
+  String kategori; // roll_on, 20ml, 30ml
+
+  Produk({
+    required this.id,
+    required this.nama,
+    required this.kategori,
+  });
+}
