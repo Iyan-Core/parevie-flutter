dec439c722a6db4a4e90dedb0cdae256698e0a5f
Iyan-Core <awianton2@gmail.com>
2026-04-03 22:02:53 +0700

    Create toko.dart

---
+++ lib/models/toko.dart
@@ -0,0 +1,13 @@
+class Toko {
+  String id;
+  String nama;
+  String area;
+  String noHp;
+
+  Toko({
+    required this.id,
+    required this.nama,
+    required this.area,
+    required this.noHp,
+  });
+}
